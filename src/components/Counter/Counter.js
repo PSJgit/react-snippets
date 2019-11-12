3c7c2892fb0a48bfccce799b78d0f8d92d022bdd
Patrick Jones <patrick.jones@comparethemarket.com.au>
2019-11-12 15:05:12 +1000

    input for counter

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -1,17 +1,26 @@
 import React, { useState, useEffect } from 'react';
 import { Button } from '@material-ui/core';
+import TextField from '@material-ui/core/TextField';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles(theme => ({
   button: {
     margin: theme.spacing(1),
-  }
+  },
+  textField: {
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1),
+    marginTop: theme.spacing(1),
+    width: 200,
+    display: 'block',
+  },
 }));
 
 const Counter = (props) => {
 
   const classes = useStyles();
   const [ count, setCount ] = useState(props.count);
+  const [ text, setText ] = useState('');
 
   useEffect(() => {
     
@@ -19,10 +28,12 @@ const Counter = (props) => {
 
   return (
     <>
-      <p>The current count is {count}</p>
-      <Button variant="contained" color="primary" className={classes.button} onClick={() => {setCount(count + 1)}}>+1</Button>
+      <p>The current {text || 'count'} is {count}</p>
       <Button variant="contained" color="primary" className={classes.button} onClick={() => {setCount(count - 1)}}>-1</Button>
       <Button variant="contained" color="primary" className={classes.button} onClick={() => {setCount(0)}}>reset</Button>
+      <Button variant="contained" color="primary" className={classes.button} onClick={() => {setCount(count + 1)}}>+1</Button>
+
+      <TextField id="counter-input" className={classes.textField} label="counter" value={text} onChange={(e) => setText(e.target.value)}/>
     </>
   )
 }
